36f7fb3f61fcce1bd5e32e8ad63a24f8ed5c0562
darienpcruz <calvillobenitezjuanantonio@gmail.com>
2025-10-30 12:04:29 -0600

    fix(login)

--- app/signin/page.js
+++ app/signin/page.js
@@ -1,16 +1,13 @@
 'use client'
 import Image from 'next/image'
-import { ShoppingCart, Menu, Search } from 'lucide-react'
+import Link from 'next/link'
 
-export default function LoginPage() {
-
+export default function SignInPage() {
   return (
     <section className="text-center">
-
-
       <div className="absolute top-0 w-full h-full flex justify-center items-center">
         <div className="relative top-0 w-full h-full flex justify-center items-center">
-          <div className="absolute left-0 w-[60%] h-full h-full bg-black/85 z-[2] items-center content-center flex justify-center rounded-r-[25%]">
+          <div className="absolute left-0 w-[60%] h-full h-full bg-black/85 z-[2] rounded-r-[300px]">
           </div>
           <Image
             src="/images/bg-signin.png"
@@ -21,22 +18,43 @@ export default function LoginPage() {
             loading="eager" />
         </div>
         <div className=" flex gap-[20px] flex-col items-center justify-center absolute top-0 z-[3] w-full h-full">
-          <div className="flex flex-col bg-white w-[40%] rounded-3xl text-black">
-            <h1 className="text-4xl py-8">Bienvenido</h1>
+          <div className="flex flex-col bg-white w-[30%] rounded-3xl text-black inset-shadow-sm inset-shadow-gray-500 shadow-xl/20">
+            <h1 className="text-4xl pt-30 pb-5">Bienvenido</h1>
             <span className="fi fi-sr-user text-8xl py-8"></span>
             <form className="flex flex-col items-center justify-center w-full text-black">
-              <div class="flex">
-  <span class="inline-flex items-center px-3 text-sm text-gray-900 bg-gray-200 border rounded-e-0 border-gray-300 border-e-0 rounded-s-md dark:bg-gray-600 dark:text-gray-400 dark:border-gray-600">
-    <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-        <path d="M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm0 5a3 3 0 1 1 0 6 3 3 0 0 1 0-6Zm0 13a8.949 8.949 0 0 1-4.951-1.488A3.987 3.987 0 0 1 9 13h2a3.987 3.987 0 0 1 3.951 3.512A8.949 8.949 0 0 1 10 18Z"/>
-    </svg>
-  </span>
-  <input type="text" id="website-admin" class="rounded-none rounded-e-lg bg-gray-50 border text-gray-900 focus:ring-blue-500 focus:border-blue-500 block flex-1 min-w-0 w-full text-sm border-gray-300 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="elonmusk"/>
-</div>
-              <button>Iniciar sesion</button>
-              <button>Registrarse</button>
+              <div className="relative py-3 w-[45%] py-10">
+                <div className="absolute inset-y-0 start-0 flex items-center pointer-events-none">
+                  <span className="items-center px-3 text-sm w-4 h-4">
+                    <span className="fi fi-sr-user"></span>
+                  </span>
+                </div>
+                <input className="block py-2 w-full p-2.5 w-full z-20 text-gray-700 rounded-full bg-gray-200/80 text-center" placeholder="Usuario" required />
+              </div>
+              <div className="relative py-3 w-[45%] py-10">
+                <div className="absolute inset-y-0 start-0 flex items-center pointer-events-none">
+                  <span className="items-center px-3 text-sm w-4 h-4">
+                    <span className="fi fi-sr-lock"></span>
+                  </span>
+                </div>
+                <input className="block py-2 w-full p-2.5 w-full z-20 text-gray-700 rounded-full bg-gray-200/80 text-center" placeholder="Contraseña" required />
+              </div>
+              <div className="py-10">
+                <Link
+                  href="/signin"
+                  className="text-black text-xl px-5 p-3 rounded-xl bg-gradient-to-r from-[#FFE417] to-[#FDBD4A]"
+                >
+                  Iniciar sesión
+                </Link>
+              </div>
+              <div className="pt-10 pb-30">
+                <Link
+                  href="/signup"
+                  className="text-black text-xl px-5 py-2"
+                >
+                  Registrarse
+                </Link>
+              </div>
             </form>
-
           </div>
         </div>
       </div>
