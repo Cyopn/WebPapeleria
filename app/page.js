53fa50f756411caf2e526ad45c2a0a0b098d74e0
Cyopn <calvillobenitezjuanantonio@gmail.com>
2025-10-30 09:50:51 -0600

    feat(login)

--- app/page.js
+++ app/page.js
@@ -10,7 +10,7 @@ export default function HomePage() {
             <h1 className="text-5xl font-bold text-white pt-10">Imprime tu mundo con color, creatividad y calidad</h1>
             <h3 className="text-4xl font-bold text-white pt-10">"Papeleria con proposito"</h3>
             <Link
-              href="/login"
+              href="/"
               className="text-black text-3xl font-bold px-5 py-2 rounded-full bg-gradient-to-r from-[#7BCE6D] to-[#A8D860] mt-10 inline-block">
               Explora nuestros productos
             </Link>
@@ -27,7 +27,7 @@ export default function HomePage() {
 
       <div className="h-[60vh] flex items-center justify-evenly flex-row text-black">
         <div class="flex flex-col justify-start rounded-xl shadow-xl/20 relative w-[20%] h-[80%] overflow-hidden transition-shadow">
-          <div class="relative flex items-center justify-center flex-row content-center bg-gray-500 h-7/10">
+          <div class="relative flex items-center justify-center flex-row content-center bg-gray-500/20 h-7/10 inset-shadow-sm inset-shadow-gray-500">
             <Image src="/images/no-image.png"
               alt="img"
               className="h-[80%]"
@@ -51,7 +51,31 @@ export default function HomePage() {
           </div>
         </div>
         <div class="flex flex-col justify-start rounded-xl shadow-xl/20 relative w-[20%] h-[80%] overflow-hidden transition-shadow">
-          <div class="relative flex items-center justify-center flex-row content-center bg-gray-500 h-7/10">
+          <div class="relative flex items-center justify-center flex-row content-center bg-gray-500/20 h-7/10 inset-shadow-sm inset-shadow-gray-500">
+            <Image src="/images/no-image.png"
+              alt="img"
+              className="h-[80%] w-[80%]"
+              width={300}
+              height={300}
+              loading="eager" />
+          </div>
+          <div class="p-4 h-3/10 w-[100%] flex gap-0 items-center justify-center flex-row content-center">
+            <div className="w-2/4">
+              <h3 className="text-xl font-medium mb-2">Product Title</h3>
+              <p className="text-lg text-gray-600">Brief description of the product.</p>
+            </div>
+            <div className="w-1/4 flex gap-0 justify-center items-center content-center">
+              <span className="font-bold text-2xl">$19.99</span>
+            </div>
+            <div className="w-1/4 flex gap-0 justify-center items-center content-center">
+              <button className="h-full w-full">
+                <i className="fi fi-rr-shopping-cart-add text-3xl"></i>
+              </button>
+            </div>
+          </div>
+        </div>
+        <div class="flex flex-col justify-start rounded-xl shadow-xl/20 relative w-[20%] h-[80%] overflow-hidden transition-shadow">
+          <div class="relative flex items-center justify-center flex-row content-center bg-gray-500/20 h-7/10 inset-shadow-sm inset-shadow-gray-500">
             <Image src="/images/no-image.png"
               alt="img"
               className="h-[80%]"
@@ -75,31 +99,7 @@ export default function HomePage() {
           </div>
         </div>
         <div class="flex flex-col justify-start rounded-xl shadow-xl/20 relative w-[20%] h-[80%] overflow-hidden transition-shadow">
-          <div class="relative flex items-center justify-center flex-row content-center bg-gray-500 h-7/10">
-            <Image src="/images/no-image.png"
-              alt="img"
-              className="h-[80%]"
-              width={300}
-              height={300}
-              loading="eager" />
-          </div>
-          <div class="p-4 h-3/10 w-[100%] flex gap-0 items-center justify-center flex-row content-center">
-            <div className="w-2/4">
-              <h3 className="text-xl font-medium mb-2">Product Title</h3>
-              <p className="text-lg text-gray-600">Brief description of the product.</p>
-            </div>
-            <div className="w-1/4 flex gap-0 justify-center items-center content-center">
-              <span className="font-bold text-2xl">$19.99</span>
-            </div>
-            <div className="w-1/4 flex gap-0 justify-center items-center content-center">
-              <button className="h-full w-full">
-                <i className="fi fi-rr-shopping-cart-add text-3xl"></i>
-              </button>
-            </div>
-          </div>
-        </div>
-        <div class="flex flex-col justify-start rounded-xl shadow-xl/20 relative w-[20%] h-[80%] overflow-hidden transition-shadow">
-          <div class="relative flex items-center justify-center flex-row content-center bg-gray-500 h-7/10">
+          <div class="relative flex items-center justify-center flex-row content-center bg-gray-500/20 h-7/10 inset-shadow-sm inset-shadow-gray-500">
             <Image src="/images/no-image.png"
               alt="img"
               className="h-[80%]"
--- app/signin/page.js
+++ app/signin/page.js
@@ -1,38 +1,45 @@
-// app/login/page.jsx
 'use client'
-import { useState } from 'react'
+import Image from 'next/image'
+import { ShoppingCart, Menu, Search } from 'lucide-react'
 
 export default function LoginPage() {
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    console.log({ email, password })
-  }
 
   return (
-    <section className="max-w-md mx-auto mt-10">
-      <h2 className="text-2xl font-semibold mb-4">Iniciar sesión</h2>
-      <form onSubmit={handleSubmit} className="flex flex-col gap-3">
-        <input
-          type="email"
-          placeholder="Correo electrónico"
-          className="border p-2 rounded"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <input
-          type="password"
-          placeholder="Contraseña"
-          className="border p-2 rounded"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <button className="bg-blue-600 text-white p-2 rounded hover:bg-blue-700">
-          Entrar
-        </button>
-      </form>
+    <section className="text-center">
+
+
+      <div className="absolute top-0 w-full h-full flex justify-center items-center">
+        <div className="relative top-0 w-full h-full flex justify-center items-center">
+          <div className="absolute left-0 w-[60%] h-full h-full bg-black/85 z-[2] items-center content-center flex justify-center rounded-r-[25%]">
+          </div>
+          <Image
+            src="/images/bg-signin.png"
+            alt="bg"
+            className="absolute w-full h-full object-cover z-[1]"
+            width={1055}
+            height={1562}
+            loading="eager" />
+        </div>
+        <div className=" flex gap-[20px] flex-col items-center justify-center absolute top-0 z-[3] w-full h-full">
+          <div className="flex flex-col bg-white w-[40%] rounded-3xl text-black">
+            <h1 className="text-4xl py-8">Bienvenido</h1>
+            <span className="fi fi-sr-user text-8xl py-8"></span>
+            <form className="flex flex-col items-center justify-center w-full text-black">
+              <div class="flex">
+  <span class="inline-flex items-center px-3 text-sm text-gray-900 bg-gray-200 border rounded-e-0 border-gray-300 border-e-0 rounded-s-md dark:bg-gray-600 dark:text-gray-400 dark:border-gray-600">
+    <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+        <path d="M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm0 5a3 3 0 1 1 0 6 3 3 0 0 1 0-6Zm0 13a8.949 8.949 0 0 1-4.951-1.488A3.987 3.987 0 0 1 9 13h2a3.987 3.987 0 0 1 3.951 3.512A8.949 8.949 0 0 1 10 18Z"/>
+    </svg>
+  </span>
+  <input type="text" id="website-admin" class="rounded-none rounded-e-lg bg-gray-50 border text-gray-900 focus:ring-blue-500 focus:border-blue-500 block flex-1 min-w-0 w-full text-sm border-gray-300 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="elonmusk"/>
+</div>
+              <button>Iniciar sesion</button>
+              <button>Registrarse</button>
+            </form>
+
+          </div>
+        </div>
+      </div>
     </section>
   )
 }
