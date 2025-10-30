6f9e521c0561f33257af9f700de4e451c2e1c3e1
darienpcruz <calvillobenitezjuanantonio@gmail.com>
2025-10-30 13:24:32 -0600

    feat(product_card)

--- app/page.js
+++ app/page.js
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import Link from 'next/link'
+import ProductCard from "@/components/product_card";
 
 export default function HomePage() {
   return (
@@ -26,102 +27,10 @@ export default function HomePage() {
       </div>
 
       <div className="h-[60vh] flex items-center justify-evenly flex-row text-black">
-        <div className="flex flex-col justify-start rounded-xl shadow-xl/20 relative w-[20%] h-[80%] overflow-hidden transition-shadow transform transition-transform duration-300 hover:scale-102">
-          <div className="relative flex items-center justify-center flex-row content-center bg-gray-500/20 h-7/10 inset-shadow-sm inset-shadow-gray-500">
-            <Image src="/images/no-image.png"
-              alt="img"
-              className="h-[80%]"
-              width={300}
-              height={300}
-              loading="eager" />
-          </div>
-          <div className="p-4 h-3/10 w-[100%] flex gap-0 items-center justify-center flex-row content-center">
-            <div className="w-2/4">
-              <h3 className="text-xl font-medium mb-2">Product Title</h3>
-              <p className="text-lg text-gray-600">Brief description of the product.</p>
-            </div>
-            <div className="w-1/4 flex gap-0 justify-center items-center content-center">
-              <span className="font-bold text-2xl">$19.99</span>
-            </div>
-            <div className="w-1/4 flex gap-0 justify-center items-center content-center">
-              <button className="h-full w-full">
-                <i className="fi fi-rr-shopping-cart-add text-3xl"></i>
-              </button>
-            </div>
-          </div>
-        </div>
-        <div className="flex flex-col justify-start rounded-xl shadow-xl/20 relative w-[20%] h-[80%] overflow-hidden transition-shadow transform transition-transform duration-300 hover:scale-102">
-          <div className="relative flex items-center justify-center flex-row content-center bg-gray-500/20 h-7/10 inset-shadow-sm inset-shadow-gray-500">
-            <Image src="/images/no-image.png"
-              alt="img"
-              className="h-[80%] w-[80%]"
-              width={300}
-              height={300}
-              loading="eager" />
-          </div>
-          <div className="p-4 h-3/10 w-[100%] flex gap-0 items-center justify-center flex-row content-center">
-            <div className="w-2/4">
-              <h3 className="text-xl font-medium mb-2">Product Title</h3>
-              <p className="text-lg text-gray-600">Brief description of the product.</p>
-            </div>
-            <div className="w-1/4 flex gap-0 justify-center items-center content-center">
-              <span className="font-bold text-2xl">$19.99</span>
-            </div>
-            <div className="w-1/4 flex gap-0 justify-center items-center content-center">
-              <button className="h-full w-full">
-                <i className="fi fi-rr-shopping-cart-add text-3xl"></i>
-              </button>
-            </div>
-          </div>
-        </div>
-        <div className="flex flex-col justify-start rounded-xl shadow-xl/20 relative w-[20%] h-[80%] overflow-hidden transition-shadow transform transition-transform duration-300 hover:scale-102">
-          <div className="relative flex items-center justify-center flex-row content-center bg-gray-500/20 h-7/10 inset-shadow-sm inset-shadow-gray-500">
-            <Image src="/images/no-image.png"
-              alt="img"
-              className="h-[80%]"
-              width={300}
-              height={300}
-              loading="eager" />
-          </div>
-          <div class="p-4 h-3/10 w-[100%] flex gap-0 items-center justify-center flex-row content-center">
-            <div className="w-2/4">
-              <h3 className="text-xl font-medium mb-2">Product Title</h3>
-              <p className="text-lg text-gray-600">Brief description of the product.</p>
-            </div>
-            <div className="w-1/4 flex gap-0 justify-center items-center content-center">
-              <span className="font-bold text-2xl">$19.99</span>
-            </div>
-            <div className="w-1/4 flex gap-0 justify-center items-center content-center">
-              <button className="h-full w-full">
-                <i className="fi fi-rr-shopping-cart-add text-3xl"></i>
-              </button>
-            </div>
-          </div>
-        </div>
-        <div className="flex flex-col justify-start rounded-xl shadow-xl/20 relative w-[20%] h-[80%] overflow-hidden transition-shadow transform transition-transform duration-300 hover:scale-102">
-          <div className="relative flex items-center justify-center flex-row content-center bg-gray-500/20 h-7/10 inset-shadow-sm inset-shadow-gray-500">
-            <Image src="/images/no-image.png"
-              alt="img"
-              className="h-[80%]"
-              width={300}
-              height={300}
-              loading="eager" />
-          </div>
-          <div className="p-4 h-3/10 w-[100%] flex gap-0 items-center justify-center flex-row content-center">
-            <div className="w-2/4">
-              <h3 className="text-xl font-medium mb-2">Product Title</h3>
-              <p className="text-lg text-gray-600">Brief description of the product.</p>
-            </div>
-            <div className="w-1/4 flex gap-0 justify-center items-center content-center">
-              <span className="font-bold text-2xl">$19.99</span>
-            </div>
-            <div className="w-1/4 flex gap-0 justify-center items-center content-center">
-              <button className="h-full w-full">
-                <i className="fi fi-rr-shopping-cart-add text-3xl"></i>
-              </button>
-            </div>
-          </div>
-        </div>
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
       </div>
     </section>
   )
