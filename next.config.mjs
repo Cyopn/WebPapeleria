319de6eabdc72f5218cb250df9845fe83a830552
Cyopn <2021452176@teschi.edu.mx>
2025-12-25 20:33:14 -0600

    fix(config)

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // allow common local dev origins and the LAN IP so other devices can load /_next/* during dev
+  middlewareClientMaxBodySize: 50 * 1024 * 1024,
+  experimental: {
+    proxyClientMaxBodySize: '50mb',
+  },
   allowedDevOrigins: [
     'local-origin.dev',
     '*.local-origin.dev',
-    // LAN device - adjust or add ports as needed
     'http://192.168.1.13',
     'http://192.168.1.13:3000',
     'http://192.168.1.13:5173',
     'http://192.168.1.13:5174',
     'https://192.168.1.13',
     'https://192.168.1.13:3000',
-    // your other device/origin that requested /_next/*
     'http://192.168.1.87',
     'http://192.168.1.87:3000',
     'http://192.168.1.87:5173',
